2692e636df4732e586ea08307c92f5b99b5ab76b
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2026-02-06 12:36:53 +0530

    init

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl">SIGNIN</h1>
+    </div>
+  );
+}
+
+export default page
